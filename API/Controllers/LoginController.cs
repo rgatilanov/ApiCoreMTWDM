5b38747e323fbcead91fa1c20caf81a85962e3b4
Gerardo <Villalobos>
2020-10-16 14:28:25 -0500

    Creación de LoginController y JWT

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="usMin">{"Nick": "rgatilanov","Password": "96CAE35CE8A9B0244178BF28E4966C2CE1B8385723A96A6B838858CDD6CA0A1E"}</param>
+        /// <returns></returns>
+        [HttpPost]
+        public Login Authenticate(UserMin usMin)
+        {
+            // Integración a base de datos
+            if (usMin.Nick == "rgatilanov" && usMin.Password == "96CAE35CE8A9B0244178BF28E4966C2CE1B8385723A96A6B838858CDD6CA0A1E") //SHA2
+            {
+                // Leemos el secret_key desde nuestro appseting
+                var secretKey = _configuration.GetValue<string>("SecretKey");
+                var key = Encoding.ASCII.GetBytes(secretKey);
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    // Nuestro token va a durar un día
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    // Credenciales para generar el token usando nuestro secretykey y el algoritmo hash 256
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var createdToken = tokenHandler.CreateToken(tokenDescriptor);
+
+                return new Login()
+                {
+                    ID = usMin.ID,
+                    Nick = usMin.Nick,
+                    Token = tokenHandler.WriteToken(createdToken),
+                };
+            }
+            else
+                return null;
+        }
+    }
+}
