4e037757f0e2fcb4c5b2f29bb79fc773f61b73e2
Gerardo <Villalobos>
2020-10-16 10:58:06 -0500

    Creación de Models, Helpers y Controller

---
+++ API/Helpers/Functions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class Functions
+    {
+        public static string GetSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+
+        public static string Md5Hash(string input)
+        {
+            // Creamos una nueva instancias
+            MD5 md5Hasher = MD5.Create();
+
+            // le sacamos los byte a la cadea
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+
+            //Creamos un string builder para aterrizar la cadena
+            StringBuilder sBuilder = new StringBuilder();
+
+            // recorremos byte por byte hasta que se transforme toda en una cadena hex
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // la regresamos
+            return sBuilder.ToString();
+        }
+    }
+}
---
+++ API/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class UserMin
+    {
+        public int ID { get; set; }
+        public string Nick { get; set; }
+        public string Password { get; set; }
+    }
+    public class User : UserMin
+    {
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public AccountType accountType { get; set; }
+    }
+
+    public enum AccountType : int
+    {
+        Basic = 0,
+        Administrator = 1,
+    }
+}
